a7dacf1ff1364bdf8f8dccac8ca46b77f9b0e8b3
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-09 14:33:41 -0400

    Complete assignment v.02 added shorter official solution to my working initial solution

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,21 +14,15 @@ class String
     self.end_with?("!")
   end
 
-  def count_sentences
+  def count_sentences_0
     raw = self.split(/\. |\! |\? /)
     return 0 if raw == []
-    # puts raw.size
     returns = raw.size
-    # puts raw.last[-1]
-    # puts raw.last[-1].match(/\.|\!|\?/)
     returns -= 1 if raw.last[-1].match(/\.|\!|\?/) == nil #!= "." && "?" && "!"
-    #   puts "hi              ht            ht                ht           ht"
-    #   returns = returns - 1
-    # end
-    # returns = 0 if returns == -1
-    # binding.pry
     returns
   end
-end
 
-puts "Y. Yout. ty! tyh??.".count_sentences
+  def count_sentences
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+  end
+end
