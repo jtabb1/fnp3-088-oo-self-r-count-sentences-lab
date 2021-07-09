932c2312d3064906930fc05c104946c611b85d6e
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-09 14:31:55 -0400

    Complete assignment v.01

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    raw = self.split(/\. |\! |\? /)
+    return 0 if raw == []
+    # puts raw.size
+    returns = raw.size
+    # puts raw.last[-1]
+    # puts raw.last[-1].match(/\.|\!|\?/)
+    returns -= 1 if raw.last[-1].match(/\.|\!|\?/) == nil #!= "." && "?" && "!"
+    #   puts "hi              ht            ht                ht           ht"
+    #   returns = returns - 1
+    # end
+    # returns = 0 if returns == -1
+    # binding.pry
+    returns
   end
 end
+
+puts "Y. Yout. ty! tyh??.".count_sentences
